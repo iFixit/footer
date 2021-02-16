7c1e619fad0ef6ae1ac79246f987dbfef0b5b1b3
macs <massimodemarchi91@gmail.com>
2021-02-16 17:12:53 +0100

    project clean up

--- src/FooterList/index.tsx
+++ src/FooterList/index.tsx
@@ -1,13 +1,11 @@
-import { fontWeight, space } from '@core-ds/primitives';
+import { breakpoint, color, fontWeight, space } from '@core-ds/primitives';
 import * as React from 'react';
 import styled from 'styled-components';
 import { Link } from '../Link';
-import { LayoutProps } from '../types';
-import { layout } from '../utils';
 
-export interface FooterListItemProps {
+export type FooterListItemProps = React.PropsWithChildren<{
    className?: string;
-}
+}>;
 
 export const FooterListItem = styled.li<FooterListItemProps>`
    display: flex;
@@ -19,11 +17,9 @@ export const FooterListItem = styled.li<FooterListItemProps>`
    }
 `;
 
-export type FooterListProps = React.PropsWithChildren<
-   {
-      className?: string;
-   } & LayoutProps
->;
+export type FooterListProps = React.PropsWithChildren<{
+   className?: string;
+}>;
 
 export const FooterList = styled.ul.attrs<FooterListProps>({
    role: 'list',
@@ -31,8 +27,13 @@ export const FooterList = styled.ul.attrs<FooterListProps>({
    margin: 0;
    padding: 0;
    list-style: none;
-   ${layout};
+   border-bottom: 1px solid ${color.gray8};
+   padding-bottom: ${space[4]};
    ${Link} {
       font-weight: ${fontWeight.bold};
+   }
+   @media screen and (min-width: ${breakpoint.sm}) {
+      border-bottom: none;
+      padding-bottom: 0;
    }
 `;
